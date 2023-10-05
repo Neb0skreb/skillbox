ea7747d30393c866486d4f95eaa52f3b9f48df53
Neb0skreb <p.zinatulin@traceair.net>
2023-10-05 22:23:00 +0300

    module 4

--- 4/02_global_color.py
+++ 4/02_global_color.py
@@ -14,6 +14,65 @@ import simple_draw as sd
 # и константы COLOR_RED, COLOR_ORANGE, COLOR_YELLOW, COLOR_GREEN, COLOR_CYAN, COLOR_BLUE, COLOR_PURPLE
 # Результат решения см lesson_004/results/exercise_02_global_color.jpg
 
-# TODO здесь ваш код
+def get_color(number):
+    colors = {
+        0: sd.COLOR_RED,
+        1: sd.COLOR_ORANGE,
+        2: sd.COLOR_YELLOW,
+        3: sd.COLOR_GREEN,
+        4: sd.COLOR_CYAN,
+        5: sd.COLOR_BLUE,
+        6: sd.COLOR_PURPLE,
+    }
+    return colors.get(number, sd.COLOR_YELLOW)  # Возвращаем желтый цвет по умолчанию
+
+def input_color():
+    print("Possible colors:")
+    print("0: red")
+    print("1: orange")
+    print("2: yellow")
+    print("3: green")
+    print("4: cyan")
+    print("5: blue")
+    print("6: purple")
+
+    while True:
+        try:
+            user_input = int(input("Enter a number for shapes color > "))
+            if 0 <= user_input <= 6:
+                return user_input
+            else:
+                print("Invalid input, must be a number between 0 - 6")
+        except ValueError:
+            print("Invalid input, must be a number")
+
+def draw_shape(point_bottom_left, lines_count=3, angle=0, length=100, width=3, color=sd.COLOR_YELLOW):
+    figure_angle = (360 / lines_count)
+    v1 = None
+
+    for i in range(lines_count):
+        if v1:
+            if i == lines_count - 1:
+                sd.line(start_point=v1.end_point, end_point=point_bottom_left, width=width, color=color)
+            else:
+                v1 = sd.get_vector(start_point=v1.end_point, angle=angle + i * figure_angle, length=length, width=width)
+            v1.draw(color=color)
+        else:
+            v1 = sd.get_vector(start_point=point_bottom_left, angle=angle, length=length, width=width)
+            v1.draw(color=color)
+
+point1 = sd.get_point(50, 50)
+point2 = sd.get_point(380, 50)
+point3 = sd.get_point(150, 380)
+point4 = sd.get_point(450, 380)
+point5 = sd.get_point(200, 200)
+
+user_color = input_color()
+color_tuple = get_color(number=user_color)
+
+draw_shape(point_bottom_left=point1, lines_count=3, angle=30, length=110, width=5, color=color_tuple)
+draw_shape(point_bottom_left=point2, lines_count=4, angle=20, length=110, width=5, color=color_tuple)
+draw_shape(point_bottom_left=point3, lines_count=5, angle=60, length=110, width=5, color=color_tuple)
+draw_shape(point_bottom_left=point4, lines_count=6, angle=35, length=110, width=5, color=color_tuple)
 
 sd.pause()
--- 4/03_shape_select.py
+++ 4/03_shape_select.py
@@ -9,6 +9,49 @@ import simple_draw as sd
 # Код функций из упр lesson_004/02_global_color.py скопировать сюда
 # Результат решения см lesson_004/results/exercise_03_shape_select.jpg
 
-# TODO здесь ваш код
+def draw_shape(point_bottom_left, lines_count=3, angle=0, length=100, width=3, color=sd.COLOR_YELLOW):
+    figure_angle = (360 / lines_count)
+    v1 = None
+
+    for i in range(lines_count):
+        if v1:
+            if i == lines_count - 1:
+                sd.line(start_point=v1.end_point, end_point=point_bottom_left, width=width, color=color)
+            else:
+                v1 = sd.get_vector(start_point=v1.end_point, angle=angle + i * figure_angle, length=length, width=width)
+            v1.draw(color=color)
+        else:
+            v1 = sd.get_vector(start_point=point_bottom_left, angle=angle, length=length, width=width)
+            v1.draw(color=color)
+
+def draw_figure(point_middle, figure_type, length=100, color=sd.COLOR_YELLOW):
+    figures = {
+        0: lambda: draw_shape(point_bottom_left=point_middle, lines_count=3, length=length, width=5, color=color),
+        1: lambda: draw_shape(point_bottom_left=point_middle, lines_count=4, length=length, width=5, color=color),
+        2: lambda: draw_shape(point_bottom_left=point_middle, lines_count=5, length=length, width=5, color=color),
+        3: lambda: draw_shape(point_bottom_left=point_middle, lines_count=6, length=length, width=5, color=color),
+    }
+    draw_function = figures.get(figure_type)
+    if draw_function:
+        draw_function()
+
+print("Possible shapes:")
+print("0: triangle")
+print("1: square")
+print("2: pentagon")
+print("3: hexagon")
+
+while True:
+    try:
+        user_input = int(input("Enter a number for a shape > "))
+        if 0 <= user_input <= 3:
+            break
+        else:
+            print("Invalid input, must be a number between 0 - 3")
+    except ValueError:
+        print("Invalid input, must be a number")
+
+point_middle = sd.get_point(250, 250)
+draw_figure(point_middle, user_input, length=110)
 
 sd.pause()
